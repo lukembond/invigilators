c5f554da7d1590e5ac6b06e5f48ed34096695acc
lukembond <luke.m.bond@gmail.com>
2015-04-12 13:46:29 +0100

    Favicon and CSS updates

--- js/application.js
+++ js/application.js
@@ -11,6 +11,21 @@ if (!(window.console && console.log)) {
     }());
 }
 
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '627980390669845',
+    xfbml      : true,
+    version    : 'v2.3'
+  });
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
 var STELLARJS = {
     init: function() {
 
